0173bf95578f7272025007dec045b66d8228046f
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-02-10 10:22:24 +0900

    アイコンファイルを更新し、不要なアイコンを削除。ヘッダーコンポーネントのスタイルを微調整。

--- src/pages/components/Headder.tsx
+++ src/pages/components/Headder.tsx
@@ -22,9 +22,9 @@ const Delay = ({ children, delay, giveClassName }: DelayProps) => {
 const Header = () => {
 	return (
 		<>
-			<div className="sticky top-0 z-10  min-h-[70px] items-center justify-center border-transparent pl-0 grid grid-cols-2 lg:grid-cols-3  bg-white shadow-md ">
+			<div className="sticky top-0 z-10 min-h-[70px] items-center justify-center border-transparent pl-0 grid grid-cols-2 lg:grid-cols-3 bg-white shadow-md">
 				<Delay delay={1000} giveClassName="animate__animated animate__fadeInDown animate__fast">
-					<div className=" flex cursor-pointer items-center gap-2 overflow-hidden whitespace-nowrap rounded-lg px-3 py-1.5 text-lg font-semibold">
+					<div className="flex cursor-pointer items-center gap-2 overflow-hidden whitespace-nowrap rounded-lg px-3 py-1.5 text-lg font-semibold">
 						<div className="text-token-text-secondary text-gray-700 flex-shrink-0">App</div>
 						<BoltIcon className="h-6 w-6 text-amber-400 flex-shrink-0 animate-scale-up-ver-center" />
 					</div>
